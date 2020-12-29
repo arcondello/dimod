aa7ce22132082790f83cf5c5bb77d58fa95f4d93
Alexander Condello <arcondello@gmail.com>
2020-12-29 14:24:16 -0800

    Reimplement AdjVectorBQM in C++
    Currently it is named AdjVectorBQM2 for testing.

---
+++ dimod/include/dimod/adjvectorbqm2.h
@@ -0,0 +1,359 @@
+// Copyright 2020 D-Wave Systems Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#pragma once
+
+#include <algorithm>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+#include "dimod/utils.h"
+
+namespace dimod {
+
+template <class Bias>
+class AdjVectorBQM2 {
+ public:
+    using bias_type = Bias;
+    using size_type = std::size_t;
+    using variable_type = std::size_t;
+
+    using neighborhood_type =
+            typename std::vector<std::pair<variable_type, bias_type>>;
+    using neighborhood_iterator = typename neighborhood_type::iterator;
+    using const_neighborhood_iterator =
+            typename neighborhood_type::const_iterator;
+
+    AdjVectorBQM2() {}
+
+    /**
+     * Construct a BQM from a dense array.
+     *
+     * @param dense An array containing the biases. Assumed to contain
+     *     `num_variables`^2 elements. The upper and lower triangle are summed.
+     * @param num_variables The number of variables.
+     */
+    template <class T>
+    AdjVectorBQM2(const T dense[], size_type num_variables,
+                  bool ignore_diagonal = false) {
+        // we know how big our linear is going to be
+        adj_.resize(num_variables);
+
+        bias_type qbias;
+
+        if (!ignore_diagonal) {
+            for (size_type v = 0; v < num_variables; ++v) {
+                adj_[v].second = dense[v * (num_variables + 1)];
+            }
+        }
+
+        for (size_type u = 0; u < num_variables; ++u) {
+            for (size_type v = u + 1; v < num_variables; ++v) {
+                qbias = dense[u * num_variables + v] +
+                        dense[v * num_variables + u];
+
+                if (qbias != 0) {
+                    adj_[u].first.emplace_back(v, qbias);
+                    adj_[v].first.emplace_back(u, qbias);
+                }
+            }
+        }
+    }
+
+    /**
+     * Construct a BQM from COO-formated iterators.
+     *
+     * A sparse BQM encoded in [COOrdinate] format is specified by three
+     * arrays of (row, column, value).
+     *
+     * [COOrdinate]: https://w.wiki/n$L
+     *
+     * @param row_iterator Iterator pointing to the beginning of the row data.
+     *     Must be a random access iterator.
+     * @param col_iterator Iterator pointing to the beginning of the column
+     *     data. Must be a random access iterator.
+     * @param bias_iterator Iterator pointing to the beginning of the bias data.
+     *     Must be a random access iterator.
+     * @param length The number of (row, column, bias) entries.
+     * @param ignore_diagonal If true, entries on the diagonal of the sparse
+     *     matrix are ignored.
+     */
+    template <class ItRow, class ItCol, class ItBias>
+    AdjVectorBQM2(ItRow row_iterator, ItCol col_iterator, ItBias bias_iterator,
+                 size_type length, bool ignore_diagonal = false) {
+        // determine the number of variables so we can allocate adj
+        if (length > 0) {
+            size_type max_label = std::max(
+                    *std::max_element(row_iterator, row_iterator + length),
+                    *std::max_element(col_iterator, col_iterator + length));
+            adj_.resize(max_label + 1);
+        }
+
+        // Count the degrees and use that to reserve the neighborhood vectors
+        std::vector<size_type> degrees(adj_.size());
+        ItRow rit(row_iterator);
+        ItRow cit(col_iterator);
+        for (size_type i = 0; i < length; ++i, ++rit, ++cit) {
+            if (*rit != *cit) {
+                degrees[*rit] += 1;
+                degrees[*cit] += 1;
+            }
+        }
+        for (size_type i = 0; i < degrees.size(); ++i) {
+            adj_[i].first.reserve(degrees[i]);
+        }
+
+        // add the values to the adjacency, not worrying about order or
+        // duplicates
+        for (size_type i = 0; i < length; i++) {
+            if (*row_iterator == *col_iterator) {
+                // linear bias
+                if (!ignore_diagonal) {
+                    linear(*row_iterator) += *bias_iterator;
+                }
+            } else {
+                // quadratic bias
+                adj_[*row_iterator].first.emplace_back(*col_iterator,
+                                                      *bias_iterator);
+                adj_[*col_iterator].first.emplace_back(*row_iterator,
+                                                      *bias_iterator);
+            }
+
+            ++row_iterator;
+            ++col_iterator;
+            ++bias_iterator;
+        }
+
+        // now sort each neighborhood and remove duplicates
+        for (variable_type v = 0; v < adj_.size(); ++v) {
+            auto begin = adj_[v].first.begin();
+            auto end = adj_[v].first.end();
+            if (!std::is_sorted(begin, end)) {
+                std::sort(begin, end);
+            }
+
+            // now remove any duplicate variables, adding the biases
+            auto it = adj_[v].first.begin();
+            while (it + 1 < adj_[v].first.end()) {
+                if (it->first == (it + 1)->first) {
+                    it->second += (it + 1)->second;
+                    adj_[v].first.erase(it + 1);
+                } else {
+                    ++it;
+                }
+            }
+        }
+    }
+
+    /**
+     * Add quadratic bias to the interaction `u`, `v`.
+     *
+     * @param u A variable in the binary quadratic model.
+     * @param v A variable in the binary quadratic model.
+     * @param b The bias to be added to the interaction between `u` and `v`.
+     *
+     * The behavior of this function is undefined when either `u` or `v` is
+     * less than 0 or greater than or equal to the number of variables in the
+     * BQM.
+     */
+    void add_interaction(variable_type u, variable_type v, bias_type b = 0) {
+        auto uv = find_quadratic(u, v);
+        if (uv.second) {
+            uv.first->second += b;
+        } else {
+            adj_[u].first.emplace(uv.first, v, b);
+        }
+        auto vu = find_quadratic(v, u);
+        if (vu.second) {
+            vu.first->second += b;
+        } else {
+            adj_[v].first.emplace(vu.first, u, b);
+        }
+    }
+
+    /// Add one (disconnected) variable to the BQM and return its index.
+    variable_type add_variable() {
+        adj_.resize(adj_.size() + 1);
+        return adj_.size() - 1;
+    }
+
+    /// Return the degree of variable `v`
+    size_type degree(variable_type v) const { return adj_[v].first.size(); }
+
+    /// Return a reference to the linear bias associated with `v`.
+    bias_type& linear(variable_type v) { return adj_[v].second; }
+
+    /// Return a reference to the linear bias associated with `v`.
+    const bias_type& linear(variable_type v) const { return adj_[v].second; }
+
+    /// Return a pair of iterators - the start and end of the neighborhood
+    std::pair<const_neighborhood_iterator, const_neighborhood_iterator>
+    neighborhood(variable_type u) const {
+        return std::make_pair(adj_[u].first.cbegin(), adj_[u].first.cend());
+    }
+
+    /// Return the number of interactions in the binary quadratic model.
+    size_type num_interactions() const {
+        size_type count = 0;
+        bool remainder = 0;
+        for (auto it = adj_.begin(); it != adj_.end(); ++it) {
+            if (it->first.size() % 2 == 0) {
+                count += it->first.size() / 2;
+            } else {
+                count += (it->first.size() - 1) / 2;
+
+                if (remainder) {
+                    remainder = false;
+                    count += 1;
+                } else {
+                    remainder = true;
+                }
+            }
+        }
+        return count;
+    }
+
+    /// Return the number of variables in the binary quadratic model.
+    size_type num_variables() const { return adj_.size(); }
+
+    /**
+     * Return the quadratic bias associated with `u`, `v`.
+     *
+     * Note that this function does not return a reference, this is because
+     * each quadratic bias is stored twice.
+     *
+     * @param u A variable in the binary quadratic model.
+     * @param v A variable in the binary quadratic model.
+     * @return The quadratic bias if it exists, otherwise 0.
+     */
+    bias_type quadratic(variable_type u, variable_type v) const {
+        auto low = find_quadratic(u, v);
+        if (low.second) {
+            return low.first->second;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Return the quadratic bias associated with `u`, `v`.
+     *
+     * Note that this function does not return a reference, this is because
+     * each quadratic bias is stored twice.
+     *
+     * @param u A variable in the binary quadratic model.
+     * @param v A variable in the binary quadratic model.
+     * @return The quadratic bias if it exists, otherwise 0.
+     * @exception out_of_range If either `u` or `v` are not variables or if
+     *     they do not have an interaction then the function throws an
+     *     exception.
+     */
+    bias_type quadratic_at(variable_type u, variable_type v) const {
+        if (!(u >= 0 && u < adj_.size() && v >= 0 && v < adj_.size()))
+            throw std::out_of_range("out of range variable");
+
+        auto low = find_quadratic(u, v);
+        if (low.second) {
+            return low.first->second;
+        } else {
+            throw std::out_of_range("given variables have no interaction");
+        }
+    }
+
+    /// Remove the interaction between `u` and `v`. Return true if it existed.
+    bool remove_interaction(variable_type u, variable_type v) {
+        auto uv = find_quadratic(u, v);
+        if (uv.second) {
+            adj_[u].first.erase(uv.first);
+            // the mirror must exist
+            adj_[v].first.erase(quadratic_lb(v, u));
+        }
+        return uv.second;
+    }
+
+    /// Resize the binary quadratic model to contain n variables.
+    void resize(size_type n) {
+        if (n < adj_.size()) {
+            // Clean out any of the to-be-deleted variables from the
+            // neighborhoods.
+            // This approach is better in the dense case. In the sparse case
+            // we could determine which neighborhoods need to be trimmed rather
+            // than just doing them all.
+            for (size_type v = 0; v < n; ++v) {
+                auto low = quadratic_lb(v, n);
+                adj_[v].first.erase(low, adj_[v].first.end());
+            }
+        }
+
+        adj_.resize(n);
+    }
+
+    /**
+     * Set the quadratic bias of the interaction `u`, `v`.
+     *
+     * @param u A variable in the binary quadratic model.
+     * @param v A variable in the binary quadratic model.
+     * @param b The bias.
+     *
+     * The behavior of this function is undefined when either `u` or `v` is
+     * less than 0 or greater than or equal to the number of variables in the
+     * BQM.
+     */
+    void set_quadratic(variable_type u, variable_type v, bias_type b) {
+        auto uv = find_quadratic(u, v);
+        if (uv.second) {
+            uv.first->second = b;
+        } else {
+            adj_[u].first.emplace(uv.first, v, b);
+        }
+        auto vu = find_quadratic(v, u);
+        if (vu.second) {
+            vu.first->second = b;
+        } else {
+            adj_[v].first.emplace(vu.first, u, b);
+        }
+    }
+
+ private:
+    std::vector<std::pair<neighborhood_type, bias_type>> adj_;
+
+    std::pair<neighborhood_iterator, bool> find_quadratic(variable_type u,
+                                                          variable_type v) {
+        auto low = quadratic_lb(u, v);
+        return std::make_pair(low,
+                              low != adj_[u].first.end() && low->first == v);
+    }
+
+    std::pair<const_neighborhood_iterator, bool> find_quadratic(
+            variable_type u, variable_type v) const {
+        auto low = quadratic_lb(u, v);
+        return std::make_pair(low,
+                              low != adj_[u].first.cend() && low->first == v);
+    }
+
+    inline neighborhood_iterator quadratic_lb(variable_type u,
+                                              variable_type v) {
+        return std::lower_bound(adj_[u].first.begin(), adj_[u].first.end(), v,
+                                utils::comp_v<variable_type, bias_type>);
+    }
+
+    inline const_neighborhood_iterator quadratic_lb(variable_type u,
+                                                    variable_type v) const {
+        return std::lower_bound(adj_[u].first.cbegin(), adj_[u].first.cend(), v,
+                                utils::comp_v<variable_type, bias_type>);
+    }
+};
+
+}  // namespace dimod
---
+++ testscpp/tests/test_bqm2.cpp
@@ -0,0 +1,368 @@
+// Copyright 2020 D-Wave Systems Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#include <cinttypes>
+#include <stdexcept>
+#include <vector>
+
+#include "../Catch2/single_include/catch2/catch.hpp"
+#include "dimod/adjvectorbqm2.h"
+
+namespace dimod {
+
+// make it easier to swap in later
+template <class B>
+using AdjVectorBQM = AdjVectorBQM2<B>;
+
+SCENARIO("bqms can be constructed from COO iterators") {
+    GIVEN("some COO arrays") {
+        int irow[4] = {0, 2, 0, 1};
+        int icol[4] = {0, 2, 1, 2};
+        float bias[4] = {.5, -2, 2, -3};
+
+        WHEN("a bqm is constructed with 0 length") {
+            auto bqm = AdjVectorBQM<float>(&irow[0], &icol[0], &bias[0], 0);
+
+            THEN("the bqm is empty") { REQUIRE(bqm.num_variables() == 0); }
+        }
+
+        WHEN("a bqm is constructed") {
+            auto bqm = AdjVectorBQM<float>(&irow[0], &icol[0], &bias[0], 4);
+
+            THEN("is takes its values form the arrays") {
+                REQUIRE(bqm.num_variables() == 3);
+                REQUIRE(bqm.linear(0) == .5);
+                REQUIRE(bqm.linear(1) == 0.);
+                REQUIRE(bqm.linear(2) == -2);
+
+                REQUIRE(bqm.num_interactions() == 2);
+                REQUIRE(bqm.quadratic(0, 1) == 2);
+                REQUIRE(bqm.quadratic(2, 1) == -3);
+                REQUIRE_THROWS_AS(bqm.quadratic_at(0, 2), std::out_of_range);
+            }
+        }
+    }
+
+    GIVEN("some COO arrays with duplicates") {
+        int irow[6] = {0, 2, 0, 1, 0, 0};
+        int icol[6] = {0, 2, 1, 2, 1, 0};
+        float bias[6] = {.5, -2, 2, -3, 4, 1};
+
+        WHEN("a bqm is constructed") {
+            auto bqm = AdjVectorBQM<float>(&irow[0], &icol[0], &bias[0], 6);
+
+            THEN("it combines duplicate values") {
+                REQUIRE(bqm.num_variables() == 3);
+                REQUIRE(bqm.linear(0) == 1.5);
+                REQUIRE(bqm.linear(1) == 0.);
+                REQUIRE(bqm.linear(2) == -2);
+
+                REQUIRE(bqm.num_interactions() == 2);
+                REQUIRE(bqm.quadratic(0, 1) == 6);
+                REQUIRE(bqm.quadratic(2, 1) == -3);
+                REQUIRE_THROWS_AS(bqm.quadratic_at(0, 2), std::out_of_range);
+            }
+        }
+    }
+
+    GIVEN("some COO arrays with multiple duplicates") {
+        int irow[4] = {0, 1, 0, 1};
+        int icol[4] = {1, 2, 1, 0};
+        float bias[4] = {-1, 1, -2, -3};
+
+        WHEN("a bqm is constructed") {
+            auto bqm = AdjVectorBQM<float>(&irow[0], &icol[0], &bias[0], 4);
+
+            THEN("it combines duplicate values") {
+                REQUIRE(bqm.num_variables() == 3);
+                REQUIRE(bqm.linear(0) == 0);
+                REQUIRE(bqm.linear(1) == 0);
+                REQUIRE(bqm.linear(2) == 0);
+
+                REQUIRE(bqm.num_interactions() == 2);
+                REQUIRE(bqm.quadratic(0, 1) == -6);
+                REQUIRE(bqm.quadratic(1, 0) == -6);
+                REQUIRE(bqm.quadratic(2, 1) == 1);
+                REQUIRE(bqm.quadratic(1, 2) == 1);
+                REQUIRE_THROWS_AS(bqm.quadratic_at(0, 2), std::out_of_range);
+                REQUIRE_THROWS_AS(bqm.quadratic_at(2, 0), std::out_of_range);
+            }
+        }
+    }
+}
+
+SCENARIO("bqms can be resized", "[bqm]") {
+    GIVEN("A bqm with some entries") {
+        float Q[16] = {1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1};
+        auto bqm = AdjVectorBQM<float>(Q, 4);
+
+        REQUIRE(bqm.num_variables() == 4);
+        REQUIRE(bqm.num_interactions() == 4);
+
+        WHEN("the size is increased") {
+            bqm.resize(10);
+
+            THEN("more disconnected variables are added") {
+                REQUIRE(bqm.num_variables() == 10);
+                REQUIRE(bqm.num_interactions() == 4);
+
+                REQUIRE(bqm.linear(5) == 0);
+                REQUIRE(bqm.linear(6) == 0);
+                REQUIRE(bqm.linear(7) == 0);
+            }
+        }
+
+        WHEN("the size is decreased by 1") {
+            bqm.resize(3);
+
+            THEN("a variable and its interactions are removed") {
+                REQUIRE(bqm.num_variables() == 3);
+                REQUIRE(bqm.num_interactions() == 2);
+            }
+        }
+
+        WHEN("the size is descreased by 2") {
+            bqm.resize(2);
+
+            THEN("two variables and their interactions are removed") {
+                REQUIRE(bqm.num_variables() == 2);
+                REQUIRE(bqm.num_interactions() == 0);
+            }
+        }
+    }
+}
+
+SCENARIO("quadratic biases can be manipulated", "[bqm]") {
+    GIVEN("A bqm with some variables but no interactions") {
+        auto bqm = AdjVectorBQM<float>();
+        bqm.resize(10);
+
+        REQUIRE(bqm.num_variables() == 10);
+        REQUIRE(bqm.num_interactions() == 0);
+
+        WHEN("set_quadratic() is called") {
+            bqm.set_quadratic(2, 3, .5);
+
+            THEN("the quadratic bias is set") {
+                REQUIRE(bqm.quadratic(2, 3) == .5);
+                REQUIRE(bqm.quadratic(3, 2) == .5);
+                REQUIRE(bqm.num_interactions() == 1);
+            }
+
+            AND_WHEN("add_interaction() is applied to the same interaction") {
+                bqm.add_interaction(2, 3, 1);
+
+                THEN("the biases are summed") {
+                    REQUIRE(bqm.quadratic(2, 3) == 1.5);
+                    REQUIRE(bqm.quadratic(3, 2) == 1.5);
+                    REQUIRE(bqm.num_interactions() == 1);
+                }
+            }
+
+            AND_WHEN("remove_interaction() is applied") {
+                REQUIRE(bqm.remove_interaction(2, 3));
+
+                THEN("there are no interactions") {
+                    REQUIRE(bqm.num_interactions() == 0);
+                }
+            }
+        }
+
+        WHEN("add_interaction() is called on a non-existant interaciton") {
+            bqm.add_interaction(2, 3, .5);
+
+            THEN("the quadratic bias is set") {
+                REQUIRE(bqm.quadratic(2, 3) == .5);
+                REQUIRE(bqm.quadratic(3, 2) == .5);
+                REQUIRE(bqm.num_interactions() == 1);
+            }
+
+            AND_WHEN("set_quadratic() is applied to the same interaction") {
+                bqm.set_quadratic(2, 3, 1);
+
+                THEN("the biases are overwritten") {
+                    REQUIRE(bqm.quadratic(2, 3) == 1);
+                    REQUIRE(bqm.quadratic(3, 2) == 1);
+                    REQUIRE(bqm.num_interactions() == 1);
+                }
+            }
+        }
+    }
+
+    GIVEN("A dense bqm with some variables") {
+        std::vector<int> Q(100, 1);
+        auto bqm = AdjVectorBQM<float>(&Q[0], 10);
+
+        REQUIRE(bqm.num_variables() == 10);
+        REQUIRE(bqm.num_interactions() == 45);
+
+        WHEN("set_quadratic() is called") {
+            bqm.set_quadratic(2, 3, .5);
+
+            THEN("the quadratic bias is set") {
+                REQUIRE(bqm.quadratic(2, 3) == .5);
+                REQUIRE(bqm.quadratic(3, 2) == .5);
+                REQUIRE(bqm.num_interactions() == 45);
+            }
+
+            AND_WHEN("add_interaction() is applied to the same interaction") {
+                bqm.add_interaction(2, 3, 1);
+
+                THEN("the biases are summed") {
+                    REQUIRE(bqm.quadratic(2, 3) == 1.5);
+                    REQUIRE(bqm.quadratic(3, 2) == 1.5);
+                    REQUIRE(bqm.num_interactions() == 45);
+                }
+            }
+
+            AND_WHEN("remove_interaction() is applied") {
+                REQUIRE(bqm.remove_interaction(2, 3));
+
+                THEN("there is one less interactions") {
+                    REQUIRE(bqm.num_interactions() == 44);
+                }
+
+                AND_WHEN(
+                        "remove_interaction() is applied again the edge is "
+                        "gone") {
+                    REQUIRE(!bqm.remove_interaction(2, 3));
+                }
+            }
+        }
+
+        WHEN("add_interaction() is called") {
+            bqm.add_interaction(2, 3, .5);
+
+            THEN("the quadratic bias is added") {
+                REQUIRE(bqm.quadratic(2, 3) == 2.5);
+                REQUIRE(bqm.quadratic(3, 2) == 2.5);
+                REQUIRE(bqm.num_interactions() == 45);
+            }
+
+            AND_WHEN("set_quadratic() is applied to the same interaction") {
+                bqm.set_quadratic(2, 3, 1);
+
+                THEN("the biases are overwritten") {
+                    REQUIRE(bqm.quadratic(2, 3) == 1);
+                    REQUIRE(bqm.quadratic(3, 2) == 1);
+                    REQUIRE(bqm.num_interactions() == 45);
+                }
+            }
+        }
+
+        WHEN("remove_interaction is called for each edge") {
+            for (auto u = 0; u < bqm.num_variables() - 1; ++u) {
+                for (auto v = u + 1; v < bqm.num_variables(); ++v) {
+                    REQUIRE(bqm.remove_interaction(u, v));
+                }
+            }
+
+            THEN("the bqm is now empty") {
+                REQUIRE(bqm.num_interactions() == 0);
+            }
+        }
+    }
+}
+
+TEMPLATE_TEST_CASE("Scenario: bqms can be constructed from dense arrays",
+                   "[bqm]", std::int8_t, std::int16_t, std::int32_t,
+                   std::int64_t, float, double) {
+    GIVEN("A matrix as an array") {
+        TestType Q[9] = {1, 0, 3, 2, 1, 0, 1, 0, 0};
+        int num_variables = 3;
+
+        WHEN("a bqm with float biases is constructed") {
+            auto bqm = AdjVectorBQM<float>(Q, num_variables);
+
+            THEN("it gets its linear from the diagonal") {
+                REQUIRE(bqm.num_variables() == 3);
+                REQUIRE(bqm.linear(0) == 1);
+                REQUIRE(bqm.linear(1) == 1);
+                REQUIRE(bqm.linear(2) == 0);
+            }
+
+            THEN("it gets its quadratic from the off-diagonal") {
+                REQUIRE(bqm.num_interactions() == 2);
+
+                // test both forward and backward
+                REQUIRE(bqm.quadratic(0, 1) == 2);
+                REQUIRE(bqm.quadratic(1, 0) == 2);
+                REQUIRE(bqm.quadratic(0, 2) == 4);
+                REQUIRE(bqm.quadratic(2, 0) == 4);
+                REQUIRE(bqm.quadratic(1, 2) == 0);
+                REQUIRE(bqm.quadratic(2, 1) == 0);
+
+                // ignores 0s
+                REQUIRE_THROWS_AS(bqm.quadratic_at(1, 2), std::out_of_range);
+                REQUIRE_THROWS_AS(bqm.quadratic_at(2, 1), std::out_of_range);
+            }
+        }
+
+        WHEN("a bqm with float biases is constructed ignoring the diagonal") {
+            auto bqm = AdjVectorBQM<float>(Q, num_variables, true);
+
+            THEN("it has 0 bias on the diagonal") {
+                REQUIRE(bqm.num_variables() == 3);
+                REQUIRE(bqm.linear(0) == 0);
+                REQUIRE(bqm.linear(1) == 0);
+                REQUIRE(bqm.linear(2) == 0);
+            }
+
+            AND_THEN("it gets its quadratic from the off-diagonal") {
+                REQUIRE(bqm.num_interactions() == 2);
+
+                // test both forward and backward
+                REQUIRE(bqm.quadratic(0, 1) == 2);
+                REQUIRE(bqm.quadratic(1, 0) == 2);
+                REQUIRE(bqm.quadratic(0, 2) == 4);
+                REQUIRE(bqm.quadratic(2, 0) == 4);
+                REQUIRE(bqm.quadratic(1, 2) == 0);
+                REQUIRE(bqm.quadratic(2, 1) == 0);
+
+                // ignores 0s
+                REQUIRE_THROWS_AS(bqm.quadratic_at(1, 2), std::out_of_range);
+                REQUIRE_THROWS_AS(bqm.quadratic_at(2, 1), std::out_of_range);
+            }
+        }
+
+        WHEN("a bqm with int biases is constructed") {
+            auto bqm = AdjVectorBQM<int>(Q, num_variables);
+
+            THEN("it gets its linear from the diagonal") {
+                REQUIRE(bqm.num_variables() == 3);
+                REQUIRE(bqm.linear(0) == 1);
+                REQUIRE(bqm.linear(1) == 1);
+                REQUIRE(bqm.linear(2) == 0);
+            }
+
+            THEN("it gets its quadratic from the off-diagonal") {
+                REQUIRE(bqm.num_interactions() == 2);
+
+                // test both forward and backward
+                REQUIRE(bqm.quadratic(0, 1) == 2);
+                REQUIRE(bqm.quadratic(1, 0) == 2);
+                REQUIRE(bqm.quadratic(0, 2) == 4);
+                REQUIRE(bqm.quadratic(2, 0) == 4);
+                REQUIRE(bqm.quadratic(1, 2) == 0);
+                REQUIRE(bqm.quadratic(2, 1) == 0);
+
+                // ignores 0s
+                REQUIRE_THROWS_AS(bqm.quadratic_at(1, 2), std::out_of_range);
+                REQUIRE_THROWS_AS(bqm.quadratic_at(2, 1), std::out_of_range);
+            }
+        }
+    }
+}
+
+}  // namespace dimod
